6712f55f3d687ad00c574def8f4107d7dcef3428
Reynardo Alfian <85624837+whatreaper@users.noreply.github.com>
2024-11-04 03:30:23 +0000

    created database folder and added the database schema in .sql. Created a placeholder for seed.sql

---
+++ backend/db/schema.sql
@@ -0,0 +1,106 @@
+--users
+CREATE TABLE Users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--workout plans
+CREATE TABLE WorkoutPlans (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Exercises
+CREATE TABLE Exercises (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    api_id INTEGER UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    muscle_group VARCHAR(100),
+    calories_burned INTEGER,
+    image_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--workouts
+CREATE TABLE Workouts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES Users(id) NOT NULL,
+    workout_plan_id UUID REFERENCES WorkoutPlans(id) NOT NULL,
+    date DATE NOT NULL,
+    points INTEGER DEFAULT 0,
+    comments TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--workout exercises join table
+CREATE TABLE WorkoutExercises (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    workout_id UUID REFERENCES Workouts(id) NOT NULL,
+    exercise_id UUID REFERENCES Exercises(id) NOT NULL,
+    repetitions INTEGER,
+    sets INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--recipes
+CREATE TABLE Recipes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    api_id INTEGER UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    image_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--UserRecipes Join Table
+CREATE TABLE UserRecipes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES Users(id) NOT NULL,
+    recipe_id UUID REFERENCES Recipes(id) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--nutritions
+CREATE TABLE Nutrition (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES Users(id) NOT NULL,
+    recipe_id UUID REFERENCES Recipes(id) NOT NULL,
+    caloric_intake INTEGER,
+    date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Leaderboard
+CREATE TABLE Leaderboard (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES Users(id) UNIQUE NOT NULL,
+    points INTEGER DEFAULT 0,
+    streak INTEGER DEFAULT 0,
+    rank INTEGER UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Comments
+CREATE TABLE Comments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES Users(id) NOT NULL,
+    workout_id UUID REFERENCES Workouts(id),
+    recipe_id UUID REFERENCES Recipes(id),
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
