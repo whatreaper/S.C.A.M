de32da5268f9824570eebde21a406ffb026ffffa
Reynardo Alfian <reynardoalfian55@gmail.com>
2024-12-02 00:58:55 -0500

    added new ssl connection handling

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const path = require('path');
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 pool.query('SELECT NOW()', (err, res) => {
